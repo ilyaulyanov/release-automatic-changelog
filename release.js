0d09e6892e21a1e80a6a8144f7e6fc3ff17abd63
Ilya Ulyanov <ilya.wt@gmail.com>
2019-06-27 11:22:25 -0700

    feat: add release script

---
+++ release.js
@@ -0,0 +1,17 @@
+var conventionalGithubReleaser = require("conventional-github-releaser");
+
+var AUTH = {
+  url: "https://api.github.com",
+  token: process.env.CONVENTIONAL_GITHUB_RELEASER_TOKEN // change this to your own GitHub token or use an environment variable
+};
+
+conventionalGithubReleaser(
+  AUTH,
+  {
+    preset: "angular"
+  },
+  function(err, responses) {
+    console.log(err);
+    console.log(responses);
+  }
+);
